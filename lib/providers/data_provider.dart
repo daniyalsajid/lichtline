784d05afcd1a13cd0207f21c2698a583dc79e7a0
Osama Asif <osama.asif20@gmail.com>
2021-02-15 23:41:48 +0500

    carbon dioxide and kw calculation completed

--- lib/providers/data_provider.dart
+++ lib/providers/data_provider.dart
@@ -33,19 +33,48 @@ class DataProvider extends ChangeNotifier {
             _stuck *
             (_watt / 1000) *
             _electricityCostEuroKWH *
-            (i + pow(_yearIncrementPercent / 100, i));
+            (1 + pow((_yearIncrementPercent / 100), i));
         if (i == 1) {
-          _totalEnergyCosting.add(_tempCalValue);
+          _totalEnergyCosting
+              .add(double.parse(_tempCalValue.toStringAsFixed(2)));
         } else {
           int currentIndex = i;
           double _newSubtractValue =
-              _tempCalValue - _totalEnergyCosting[currentIndex - 2];
+              _totalEnergyCosting[currentIndex - 2] - _tempCalValue;
           double _newValueForNextYear =
               _newSubtractValue + _totalEnergyCosting[currentIndex - 2];
-          _totalEnergyCosting.add(_tempCalValue);
+          _totalEnergyCosting
+              .add(double.parse(_newValueForNextYear.toStringAsFixed(2)));
         }
       }
     }
     print(_totalEnergyCosting);
+  }
+
+  totalCarbonDioxide() {
+    int _hours = int.parse(_altLosung[0].value);
+    int _days = int.parse(_altLosung[1].value);
+    int _stuck = int.parse(_altLosung[2].value);
+    int _watt = int.parse(_altLosung[3].value);
+    List _totalCo2 = [];
+    for (int i = 1; i <= _year; i++) {
+      double tempCal =
+          (_hours * _days * _stuck * (_watt / 10000) * (486 / 100000)) * i;
+      _totalCo2.add(tempCal);
+    }
+    print(_totalCo2);
+  }
+
+  totalKw() {
+    int _hours = int.parse(_altLosung[0].value);
+    int _days = int.parse(_altLosung[1].value);
+    int _stuck = int.parse(_altLosung[2].value);
+    int _watt = int.parse(_altLosung[3].value);
+    List _totalKw = [];
+    for (int i = 1; i <= _year; i++) {
+      double tempCal = (_hours * _days * _stuck * (_watt / 1000)) * i;
+      _totalKw.add(tempCal);
+    }
+    print(_totalKw);
   }
 }
